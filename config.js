ef224d2dbb28bebb7e351114ba72c0009b16f870
Spoiler038 <Spoiler038@gmail.com>
2026-05-07 03:35:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 ﻿// Этот файл НЕ коммитится в git (добавлен в .gitignore)
 // Вставьте сюда URL вашего GAS Web App после деплоя
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzLsF4cuCK6kLchkoRxudpq5uD-YY66WQwLtrjIUNWIQKLHgHnVxMks2WXiRaJehCt6/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzdUek6SoPwWwY-stwAXQyQ5qt7MoCkLc0y1pdLPGZNMZiKgUWkTXXkBEp2qEk7cQsQ/exec';
